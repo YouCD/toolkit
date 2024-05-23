cf3840e735b6d711ba5f436629f89733c2618fbe
ycd <hnyoucd@gmail.com>
2024-05-23 09:51:59 +0800

    添加pprof

---
+++ pprof/pprof.go
@@ -0,0 +1,17 @@
+package pprof
+
+import (
+	"log"
+	"net/http"
+	_ "net/http/pprof"
+)
+
+func Pprof(addr string) {
+	addrTmp := addr
+	if addrTmp == "" {
+		addrTmp = ":6060"
+	}
+	if err := http.ListenAndServe(addrTmp, nil); err != nil {
+		log.Fatal(err)
+	}
+}
